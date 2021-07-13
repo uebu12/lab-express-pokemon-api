80cb386e847f0d26a16eeb2a98cd1d125bcf3664
John Doe <gabrifurquim@gmail.com>
2021-07-12 21:23:30 -0300

    not done

--- app.js
+++ app.js
@@ -8,5 +8,43 @@ const allPokemon = require("./data");
 const app = express();
 
 // -- Define your route listeners here! --
-
+app.use(express.json());
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
+
+app.get("/pokemon", (req, res) => {
+  return res.json(allPokemon);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+  const foundPokemon = allPokemon.find((pokemon) => {
+    return pokemon.id == id;
+  });
+  if (foundPokemon) {
+    return res.json(foundPokemon);
+  } else {
+    return res.json({ msg: "Pokemon not found." });
+  }
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const queryParams = req.query;
+  
+
+  for (let key in queryParams) {
+    const foundPokemon2 = allPokemon.find((pokemon) => {
+      
+
+      return pokemon[key] === queryParams[key];
+    });
+
+    if (foundPokemon2) {
+      return res.json(foundPokemon2);
+    } else {
+       
+      return res.json({ msg: "Contact not found." });
+    }
+  }
+
+  res.json(queryParams);
+});
